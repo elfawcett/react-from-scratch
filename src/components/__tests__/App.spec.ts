d310d12b8128d2b7584070e393d844fd91df25ee
Eric Fawcett <elfawcett@gmail.com>
2019-10-14 13:35:26 -0400

    5 - Add Enzyme and basic tests

--- src/components/__tests__/App.spec.ts
+++
@@ -1,9 +0,0 @@
-/* make this an integration test i guess with testing App and different Pages.
-it should know about alll of our pages then check each selection renders that
-component properly.  then intentionally break App.tsx so it fails to render
-something and the test should fail */
-describe('App test', () => {
-  test('it intentionally passes', () => {
-    expect(1).toBe(1);
-  });
-});
